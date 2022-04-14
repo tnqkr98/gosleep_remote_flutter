9cf3df0d9627387f820de0ff755d031bfd436d8e
Dong Han Park <tnqkr98@naver.com>
2022-04-14 22:30:29 +0900

    Update SleepMainWidget.dart
    1. 커스텀 라디오버튼 추가
    2. TODO : 커스텀 위젯 deprecated 된거 쓰지말고 다른거 찾을 것, 리팩토링, 이벤트 연결

--- lib/SleepMainWidget.dart
+++ lib/SleepMainWidget.dart
@@ -13,12 +13,17 @@ class SleepMainWidget extends StatefulWidget{
 
 class _SleepMainWidget extends State<SleepMainWidget>{
   @override
+  void initState() {
+
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [GosleepColors.blue_40,GosleepColors.blue_90],
+            colors: [GosleepColors.blue_50,GosleepColors.blue_90],
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter
           )
@@ -59,7 +64,7 @@ class _GosleepTitle extends State<GosleepTitle> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: const [
                 Divider(height: 10,color: Color(0x00000000),),
-                Text('   1.0.6', style: TextStyle(
+                Text('   1.0.0', style: TextStyle(
                     fontWeight: FontWeight.w400,
                     fontFamily: "Roboto_Regular",
                     fontStyle:  FontStyle.normal,
@@ -92,6 +97,20 @@ class GosleepBody extends StatefulWidget{
 class _GosleepBody extends State<GosleepBody> {
   var isAlarmOn = false;
 
+  int selectedAir = 0;
+  List<String> airList = ['강하게','보통','약하게'];
+  late List<Widget> airRadioList;
+
+  int selectedSound = 0;
+  List<String> soundList = ['4','3','2','1','0'];
+
+
+  @override
+  void initState() {
+    //for()
+    // TODO - 반복문으로 라디오 버튼 위젯 초기화 리팩토링.
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -116,33 +135,118 @@ class _GosleepBody extends State<GosleepBody> {
               ],
             ),
             const Divider(height: 15,color: Color(0x00000000),),
-            SizedBox(
+            Container(
               height: MediaQuery.of(context).size.height/5,
+              decoration: const BoxDecoration(
+                color: Color(0x11ffffff),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15.0),
+                  bottomLeft: Radius.circular(15.0)
+                )
+              ),
               child: CupertinoTheme(
                 data: const CupertinoThemeData(
                   textTheme: CupertinoTextThemeData(
-                    dateTimePickerTextStyle: TextStyle(color: Colors.white,fontSize: 25,fontFamily: 'SpoqaHanSansNeo'),
+                    dateTimePickerTextStyle: TextStyle(color: Colors.white, fontSize: 25, fontFamily: 'SpoqaHanSansNeo'),
                   ),
                 ),
                 child:CupertinoDatePicker(
                   mode:CupertinoDatePickerMode.time,
-                  backgroundColor: const Color(0x11ffffff),
                   use24hFormat: false,
                   onDateTimeChanged: (value){}
                 ),
               )
             ),
-            Row(
-              children: const [
-                Divider(height: 90,),
-                Expanded(
-                    child:Text('슬립 에어 세기',style: GosleepTextStyles.title3Medium_KOR,)
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child:Row(
+                children: const [
+                  Text('슬립 에어 세기',style: GosleepTextStyles.title3Medium_KOR,),
+                ],
+              ) ,
             ),
+            Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child:Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[0], 0),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[1], 1),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[2], 2),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child:Row(
+                children: const [
+                  Text('수면 음악 볼륨',style: GosleepTextStyles.title3Medium_KOR,),
+                ],
+              ) ,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child:Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[0], 0),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[1], 1),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: customRadio(airList[2], 2),
+                      ),
+                    ),
+                  ],
+                )
+            )
           ],
         ),
       )
     );
+  }
+
+  Widget customRadio(String txt, int index){
+    return OutlineButton(
+      onPressed: () {
+          setState((){
+            selectedAir = index;
+          });
+        },
+      padding: const EdgeInsets.all(18),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      borderSide: BorderSide(color: selectedAir == index ? Colors.cyan : Colors.grey),
+      color: Colors.cyan,
+      child: Text(txt,
+          style: TextStyle(
+              color: selectedAir == index ? Colors.cyan : Colors.grey,
+              fontFamily: 'SpoqaHanSansNeo',
+              fontSize: 16
+          ),
+        ),
+      );
   }
 }
